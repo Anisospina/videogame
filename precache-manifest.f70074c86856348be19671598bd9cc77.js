4a0681b7aec4472a4a6a4255b7b562171a9f4d56
Ana Ospina <anisospina@Anas-MacBook-Pro.local>
2020-03-13 00:49:58 -0500

    build 2

---
+++ precache-manifest.f70074c86856348be19671598bd9cc77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5511f5863e1987d9e0202a01eb5fd41",
+    "url": "/videogame/index.html"
+  },
+  {
+    "revision": "20edf979c58659b1d08a",
+    "url": "/videogame/static/css/main.6670d254.chunk.css"
+  },
+  {
+    "revision": "b5bf85ff30158de7f3a4",
+    "url": "/videogame/static/js/2.e1d5f06a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/videogame/static/js/2.e1d5f06a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20edf979c58659b1d08a",
+    "url": "/videogame/static/js/main.b8d5865f.chunk.js"
+  },
+  {
+    "revision": "a0377beaee608192e513",
+    "url": "/videogame/static/js/runtime-main.0121ad85.js"
+  },
+  {
+    "revision": "3f348266d9af6ccc77d6e1763ec2d4b2",
+    "url": "/videogame/static/media/p02.3f348266.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab8cc0ac5d877db56091144a45bce87e.js"
+  "/videogame/precache-manifest.f70074c86856348be19671598bd9cc77.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/videogame/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
