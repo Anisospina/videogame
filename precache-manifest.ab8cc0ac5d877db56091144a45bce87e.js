7e4c532a532f4c2813bc5c67ea7deaa9af8c99c8
Ana Ospina <anisospina@Anas-MacBook-Pro.local>
2020-03-13 00:41:24 -0500

    Github page initial

---
+++ precache-manifest.ab8cc0ac5d877db56091144a45bce87e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38cd07f50b3006f3e8615f595cf4b097",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3e6dc96ced6c8bc1d00",
+    "url": "/static/css/main.6670d254.chunk.css"
+  },
+  {
+    "revision": "b5bf85ff30158de7f3a4",
+    "url": "/static/js/2.e1d5f06a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e1d5f06a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3e6dc96ced6c8bc1d00",
+    "url": "/static/js/main.7a66790b.chunk.js"
+  },
+  {
+    "revision": "8a67327770eafa00cbdc",
+    "url": "/static/js/runtime-main.f88b05c5.js"
+  },
+  {
+    "revision": "3f348266d9af6ccc77d6e1763ec2d4b2",
+    "url": "/static/media/p02.3f348266.png"
+  }
+]);
